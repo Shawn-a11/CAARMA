e62f75e11c6ac2e1a8e7f0a12060c0cceb2f6d47
Shawn-a11 <shuwen8681@gmail.com>
2026-05-27 15:16:05 +0800

    Fix criterion imports, add AMSoftmax variant, add parse_log utility

--- criterion/build_criterion.py
+++ criterion/build_criterion.py
@@ -1,8 +1,11 @@
 from criterion.amsoftmax_mix_gan import amsoftmax_gan
+from criterion.amsoftmax import amsoftmax
 
 def build_criterion(config):
     if config['criterion'] == 'AMSoftmaxGAN':
-        criterion = amsoftmax_gan(embedding_dim=192, num_classes=1211, m=0.2, s=30)
+        criterion = amsoftmax_gan(embedding_dim=config['embedding_dim'], num_classes=config['num_spk'], m=0.2, s=30)
+    elif config['criterion'] == 'AMSoftmax':
+        criterion = amsoftmax(embedding_dim=config['embedding_dim'], num_classes=config['num_spk'], m=0.2, s=30)
     else:
         raise NotImplementedError
 
