20f68554578cab13d6d3aea044ba792232143263
Shawn-a11 <shuwen8681@gmail.com>
2026-06-13 18:39:06 +0800

    fix: replace torchaudio.load with soundfile.read in load_audio, and revert multiprocessing_context to default fork

--- functions/dataset.py
+++ functions/dataset.py
@@ -11,11 +11,13 @@ from sklearn.utils import shuffle
 from torch.utils.data import DataLoader, Dataset
 import soundfile as sf
 
-import torchaudio
+import soundfile as sf
 
 def load_audio(filename, second=3):
-    waveform, sr = torchaudio.load(filename)
-    waveform = waveform.squeeze(0)  # Remove channel dimension if mono
+    data, sr = sf.read(filename, dtype='float32')
+    if len(data.shape) > 1:
+        data = data[:, 0]
+    waveform = torch.from_numpy(data)
 
     audio_length = waveform.shape[0]
 
@@ -113,7 +115,7 @@ class Evaluation_Dataset(Dataset):
         waveform_lengths = torch.tensor(waveform_lengths)
         audio_paths = [item['path'] for item in batch]
 
-        audios_padded = torch.Floattensor(audios)
+        audios_padded = torch.FloatTensor(audios)
 
         return {
             "waveform": audios_padded,
